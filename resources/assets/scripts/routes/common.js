bff39b3dbf800876732347b1c720fc5b46e296d7
Guillem <guillem@disedit.com>
2020-02-21 13:20:56 +0100

    Style navbar

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,12 @@
 export default {
   init() {
-    // JavaScript to be fired on all pages
+    $(document).scroll(function() {
+      if ($(document).scrollTop() >= 200) {
+        $('.brand-local').removeClass('brand-local-hidden');
+      } else {
+        $('.brand-local').addClass('brand-local-hidden');
+      }
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,24 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">
-      <img src="https://compromis.net/wp-content/themes/Compromis/images/logo.svg" alt="Compromís" height="35" />
-      <div class="brand-local">{!! App::get_localinfo()->name !!}</div>
-    </a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
+  <div class="banner-local-image" style="background: url(https://images.unsplash.com/photo-1563273410-979e8ec6bbb7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2467&q=80)">
+    <div class="container banner-container">
+      <div class="banner-band">
+        <a class="brand" href="{{ home_url('/') }}">
+          <img src="https://compromis.net/wp-content/themes/Compromis/images/logo.svg" alt="Compromís" />
+          <span class="brand-local brand-local-hidden">{!! App::get_localinfo()->name !!}</span>
+        </a>
+        <nav class="nav-primary">
+            @if (has_nav_menu('primary_navigation'))
+              {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+            @endif
+        </nav>
+      </div>
+    </div>
+    <div class="banner-local-name container">
+      <a href="{{ home_url('/') }}">
+        <h1>{!! App::get_localinfo()->name !!}</h1>
+      </a>
+    </div>
+    <div class="blob blob-1"></div>
+    <div class="blob blob-2"></div>
   </div>
 </header>
