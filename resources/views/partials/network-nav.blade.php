10193513801b84039626c316e0755023766e2852
Guillem <guillem@disedit.com>
2020-02-26 11:30:54 +0100

    Update nav

--- resources/views/partials/network-nav.blade.php
+++ resources/views/partials/network-nav.blade.php
@@ -11,15 +11,8 @@
       <li><a href="">Iniciativa</a></li>
       <li><a href="">Verds</a></li>
     </ul>
-
-    <h3>JOVENTUTS</h3>
-    <ul>
-      <li><a href="">JovesPV-Compromís</a></li>
-      <li><a href="">Joves amb Iniciativa</a></li>
-      <li><a href="">Joves Verds-Equo</a></li>
-    </ul>
   </div>
-  <div class="col-md-3">
+  <div class="col-md-6">
     <h3>INSTITUCIONS</h3>
     <ul>
       <li><a href="">Corts Valencianes</a></li>
@@ -32,15 +25,11 @@
     </li>
   </div>
   <div class="col-md-3">
-    <h3>COMARQUES</h3>
+    <h3>JOVENTUTS</h3>
     <ul>
-      <li>...</li>
-    </ul>
-  </div>
-  <div class="col-md-3">
-    <h3>MUNICIPIS</h3>
-    <ul>
-      <li>...</li>
+      <li><a href="">JovesPV-Compromís</a></li>
+      <li><a href="">Joves amb Iniciativa</a></li>
+      <li><a href="">Joves Verds-Equo</a></li>
     </ul>
   </div>
 </div>
