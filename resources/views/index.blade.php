870f1adb17f570e96d36fcf98fd977ecb89835bf
Guillem <guillem@disedit.com>
2020-03-02 10:58:11 +0100

    Style post cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
 
   <div class="row">
   @while (have_posts()) @php the_post() @endphp
-    <div class="col-md-4">
+    <div class="col-md-4 d-flex align-items-stretch">
       @include('partials.content-'.get_post_type())
     </div>
   @endwhile
