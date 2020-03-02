1df53fb79a47c114236086ec8f1c5e69dc731ba9
Carles <carles2612@gmail.com>
2020-03-02 14:26:34 +0100

    style post footer nav

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -6,8 +6,12 @@
   <div class="entry-content">
     @php the_content() @endphp
   </div>
-  <footer>
-    {!! previous_post_link('<span class="nav-icon">&lt;</span><span>%link</span>') !!}
-    {!! next_post_link('<span>%link</span><span class="nav-icon">&gt;</span>') !!}
+  <footer class="entry-nav">
+    <div class="footer-prev">
+      {!! previous_post_link('%link', '<span class="nav-icon">&lt;-</span><span class="nav-text">%title</span>') !!}
+    </div>
+    <div class="footer-next">
+      {!! next_post_link('%link', '<span class="nav-text">%title</span><span class="nav-icon">-&gt;</span>') !!}
+    </div>
   </footer>
 </article>
