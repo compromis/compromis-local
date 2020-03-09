c1be8201adac18daaee04038ba4f7700584816fc
Guillem <guillem@disedit.com>
2020-03-09 11:49:44 +0100

    Swap alpinejs for vue

--- resources/views/partials/compromis-posts.blade.php
+++ resources/views/partials/compromis-posts.blade.php
@@ -1,24 +1,17 @@
 <h3>Posts</h3>
-<div id="compromis-posts"
-  x-data="{ posts: [] }"
-  x-init="
-    fetch('https://compromis.net/wp-json/wp/v2/posts/?_fields=title,excerpt,link,_links.wp:featuredmedia&per_page=5&_embed&categories_exclude=1939')
-      .then(response => { return response.json() })
-      .then(data => { posts = data })
-  ">
+
+<div id="compromis-posts" id="compromis-posts">
   <ul class="post-list">
-    <template x-for="post in posts" :key="post">
-      <li>
-        <a class="post-link" x-bind:href="post.link" target="_blank" rel="noopener">
-          <div class="post-thumbnail">
-            <img x-bind:src="post._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url" alt="" />
-          </div>
-          <p class="post-content">
-            <span class="post-title" x-text="post.title.rendered"></span>
-          </p>
-        </a>
-      </li>
-    </template>
+    <li v-for="post in posts" v-bind:key="post.id">
+      <a class="post-link" v-bind:href="post.link" target="_blank" rel="noopener">
+        <div class="post-thumbnail">
+          <img v-bind:src="post._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url" alt="" />
+        </div>
+        <p class="post-content">
+          <span class="post-title" v-text="post.title.rendered"></span>
+        </p>
+      </a>
+    </li>
   </ul>
 
   <a href="https://compromis.net" target="_blank" rel="noopener">{{ __('Més notícies en compromis.net', 'sage') }}</a>
--- resources/views/partials/network-nav.blade.php
+++ resources/views/partials/network-nav.blade.php
@@ -28,32 +28,18 @@
       <li><a href="">FVMP</a></li>
     </li>
   </div>
-  <div class="col-lg-6 col-md-12 mt-2 mt-md-0">
+  <div class="col-lg-6 col-md-12 mt-2 mt-md-0" id="locals">
     <h3>COMPROMÍS AL TEU MUNICIPI</h3>
-    <form class="search-form">
+    <form class="search-form" v-on:submit="filter">
       <label for="municipality" class="sr-only">Municipi</label>
-      <input type="search" name="municipality" id="municipality" class="form-control search-field" placeholder="Escriu el teu municipi" />
+      <input type="search" name="municipality" id="municipality" class="form-control search-field" placeholder="Escriu el teu municipi" v-model="filterValue" />
       <input type="submit" name="municipality_filter" class="sr-only" />
       <i class="far fa-search"></i>
     </form>
     <ul class="territorial pill-list mt-2">
-      <li><a class="pill" href="">Alacantí</a></li>
-      <li><a class="pill" href="">Alcoià - Comtat - Foies</a></li>
-      <li><a class="pill" href="">Camp de Morverdre - Alt Palància</a></li>
-      <li><a class="pill" href="">Camp de Túria - Serrans - Racó d'Ademús</a></li>
-      <li><a class="pill" href="">Comarcas del Interior</a></li>
-      <li><a class="pill" href="">Costera - Canal de Navarrés</a></li>
-      <li><a class="pill" href="">Horta Nord</a></li>
-      <li><a class="pill" href="">Maestrat - Ports</a></li>
-      <li><a class="pill" href="">Marina Alta</a></li>
-      <li><a class="pill" href="">Marina Baixa</a></li>
-      <li><a class="pill" href="">Plana Alta - Alcalatén</a></li>
-      <li><a class="pill" href="">Ribera Alta</a></li>
-      <li><a class="pill" href="">Ribera Baixa</a></li>
-      <li><a class="pill" href="">Safor - Valldigna</a></li>
-      <li><a class="pill" href="">València</a></li>
-      <li><a class="pill" href="">Vall d'Albaida</a></li>
-      <li><a class="pill" href="">Vinalopó - Segura</a></li>
+      <li v-for="local in filteredResults" v-bind:key="local.id">
+        <a class="pill" v-bind:href="local.url"><span v-text="local.name"></span></a>
+      </li>
     </ul>
   </div>
 </div>
