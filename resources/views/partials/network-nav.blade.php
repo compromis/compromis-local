aaa502c98a7ca573659829c218026cf2bd81e49d
Guillem <guillem@disedit.com>
2020-03-09 14:22:45 +0100

    Add network links

--- resources/views/partials/network-nav.blade.php
+++ resources/views/partials/network-nav.blade.php
@@ -1,31 +1,31 @@
-<h2 class="network-name"><a href="">compromís.net</a></h2>
+<h2 class="network-name"><a href="https://compromis.net">compromís.net</a></h2>
 
 <div class="row">
   <div class="col-lg-3 col-md-6">
     <h3>PARTITS</h3>
     <ul>
-      <li><a href="">BLOC</a></li>
-      <li><a href="">Iniciativa</a></li>
-      <li><a href="">Verds</a></li>
+      <li><a href="https://bloc.compromis.net">BLOC</a></li>
+      <li><a href="https://iniciativa.compromis.net">Iniciativa</a></li>
+      <li><a href="https://verds.compromis.net">Verds</a></li>
     </ul>
 
     <h3>JOVENTUTS</h3>
     <ul>
-      <li><a href="">JovesPV-Compromís</a></li>
-      <li><a href="">Joves amb Iniciativa</a></li>
-      <li><a href="">Joves Verds-Equo</a></li>
+      <li><a href="https://jovespv.org">JovesPV-Compromís</a></li>
+      <li><a href="https://jovesambiniciativa.compromis.net/">Joves amb Iniciativa</a></li>
+      <li><a href="https://www.facebook.com/jovesverdsqpv">Joves Verds-Equo</a></li>
     </ul>
   </div>
   <div class="col-lg-3 col-md-6">
     <h3>INSTITUCIONS</h3>
     <ul>
-      <li><a href="">Corts Valencianes</a></li>
-      <li><a href="">Congrés</a></li>
-      <li><a href="">Senat</a></li>
-      <li><a href="">Diputació de València</a></li>
-      <li><a href="">Diputació d'Alacant</a></li>
-      <li><a href="">Diputació de Castelló</a></li>
-      <li><a href="">FVMP</a></li>
+      <li><a href="https://corts.compromis.net">Corts Valencianes</a></li>
+      <li><a href="https://congres.compromis.net">Congrés</a></li>
+      <li><a href="https://senat.compromis.net">Senat</a></li>
+      <li><a href="https://dipval.compromis.net">Diputació de València</a></li>
+      <li><a href="https://dipalc.compromis.net">Diputació d'Alacant</a></li>
+      <li><a href="https://dipcas.compromis.net">Diputació de Castelló</a></li>
+      <li><a href="https://fvmp.compromis.net">FVMP</a></li>
     </li>
   </div>
   <div class="col-lg-6 col-md-12 mt-2 mt-md-0" id="locals">
@@ -38,7 +38,7 @@
     </form>
     <ul class="territorial pill-list mt-2">
       <li v-for="website in filteredResults" v-bind:key="website.id">
-        <a class="pill" v-bind:href="website.url" target="_blank" v-if="website.hasOwnProperty('territori_id')"><span v-text="website.name"></span></a>
+        <a class="pill" v-bind:href="website.url" v-if="website.hasOwnProperty('territori_id')"><span v-text="website.name"></span></a>
         <a class="pill" href="#" v-on:click.prevent="setTerritory(website)" v-else><span v-text="website.name"></a>
       </li>
     </ul>
