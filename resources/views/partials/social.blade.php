843e3825b5897867de0c78a4c281cb770d50a76e
Guillem <guillem@disedit.com>
2020-03-17 12:39:15 +0100

    Update social buttons

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -1,17 +1,21 @@
 <div class="social-nav">
-  <a href="{{ the_permalink() }}" id="copy-link" data-url="{{ the_permalink() }}" data-toggle="tooltip" data-placement="top" title="{{ __('Copia enllaç', 'sage') }}" data-clipboard-text="{{ the_permalink() }}">
+  <a href="{{ the_permalink() }}" id="copy-link" data-url="{{ the_permalink() }}" data-toggle="tooltip" data-placement="top" title="{{ __('Copiar enllaç', 'sage') }}" data-clipboard-text="{{ the_permalink() }}">
     <i class="far fa-link"></i>
   </a>
-  <a href="https://www.facebook.com/sharer/sharer.php?u={{ the_permalink() }}" target="_blank" rel="noopener" data-toggle="tooltip" data-placement="top" title="{{ __('Comparteix a Facebook', 'sage') }}">
+  <a href="https://www.facebook.com/sharer/sharer.php?u={{ the_permalink() }}" target="_blank" rel="noopener">
     <i class="fab fa-facebook"></i>
+    <span class="sr-only">{{ __('Comparteix per Facebook', 'sage') }}</span>
   </a>
-  <a href="https://twitter.com/intent/tweet?text={{ the_title() }} {{ the_permalink() }}" target="_blank" rel="noopener" data-toggle="tooltip" data-placement="top" title="{{ __('Comparteix a Twitter', 'sage') }}">
+  <a href="https://twitter.com/intent/tweet?text={{ the_title() }} {{ the_permalink() }}" target="_blank" rel="noopener">
     <i class="fab fa-twitter"></i>
+    <span class="sr-only">{{ __('Comparteix per Twitter', 'sage') }}</span>
   </a>
-  <a href="whatsapp://send?text=http://www.example.com" data-toggle="tooltip" data-placement="top" title="{{ __('Comparteix a Whatsapp', 'sage') }}">
+  <a href="whatsapp://send?text=http://www.example.com">
     <i class="fab fa-whatsapp"></i>
+    <span class="sr-only">{{ __('Comparteix per Whatsapp', 'sage') }}</span>
   </a>
-  <a href="mailto:destinatari@exemple.com?&subject={{ the_title() }}&body={{ the_permalink() }}" data-toggle="tooltip" data-placement="top" title="{{ __('Comparteix per e-mail', 'sage') }}">
+  <a href="mailto:destinatari@exemple.com?&subject={{ the_title() }}&body={{ the_permalink() }}">
     <i class="far fa-envelope"></i>
+    <span class="sr-only">{{ __('Comparteix per e-mail', 'sage') }}</span>
   </a>
 </div>
