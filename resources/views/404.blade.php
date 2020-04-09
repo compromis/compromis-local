507ca7c07bd42d3e3e7a2914ef02aa596d81a3d8
Guillem <guillem@disedit.com>
2020-04-09 16:51:21 +0200

    Update error page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -2,6 +2,7 @@
   <title>Compromís - Error 404</title>
   <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <style>
       @font-face {
         font-family: "Compromis";
@@ -25,51 +26,70 @@
         --body-color: #353949;
         --gray: #979aa4;
         --primary: #ff6720;
+        --gradient-start: #d23517;
+        --gradient-end: #f39128;
       }
 
       body {
+        font-family: Compromis, sans-serif;
         margin: 0;
       }
 
       h1 {
         font-size: 8rem;
+        margin: 0;
+        letter-spacing: -0.04em;
         color: var(--primary);
-        margin: 1rem 0;
+        background: linear-gradient(45deg, var(--gradient-start), var(--gradient-end));
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        width: fit-content;
       }
 
       p {
         color: var(--body-color);
         font-size: 2.25rem;
         margin: 1rem 0;
+        line-height: 1.25;
+        letter-spacing: -0.03em;
       }
 
       .container {
         display: grid;
-        grid-template-columns: .75fr 1fr;
-        font-family: Compromis, sans-serif;
-        margin: 0 auto;
-        max-width: 1200px;
-        align-content: center;
+        grid-template-columns: 1fr 1.5fr;
+        min-height: 100vh;
+      }
+
+      .careta {
+        display: flex;
         justify-content: center;
-        height: 100vh;
-        gap: 2rem;
-        padding: 0 2rem;
+        align-content: center;
+        background: linear-gradient(45deg, var(--gradient-start), var(--gradient-end));
+        padding: 3rem;
+      }
+
+      .error-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 4rem;
       }
 
       svg {
         justify-self: center;
-        width: 290px;
+        width: 100%;
+        max-width: 20vw;
         height: auto;
       }
 
-      .st1 {
-        fill: var(--gray);
-        opacity: 0.4;
+      .careta-path {
+        fill: white;
       }
 
       .error-number {
         color: var(--gray);
         font-size: 2.25rem;
+        letter-spacing: -0.03em;
       }
 
       a.apple-link {
@@ -79,7 +99,12 @@
         font-size: 1.5rem;
         font-weight: 700;
         color: var(--primary);
+        background: linear-gradient(45deg, var(--gradient-start), var(--gradient-end));
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        width: fit-content;
         text-decoration: none;
+        letter-spacing: -0.03em;
       }
 
       a.apple-link span {
@@ -103,7 +128,10 @@
       @media (max-width: 800px) {
         .container {
           grid-template-columns: 1fr;
-          padding: 0 2rem;
+        }
+
+        .error-content {
+          padding: 2rem;
         }
 
         h1 {
@@ -115,8 +143,12 @@
           font-size: 1.5rem;
         }
 
+        a.apple-link {
+          font-size: 1rem;
+        }
+
         svg {
-          justify-self: start;
+          justify-self: center;
           width: 200px;
         }
       }
@@ -125,20 +157,22 @@
 
   <body>
     <div class="container">
-      <svg x="0px" y="0px" viewBox="0 0 462 312.1" xml:space="preserve">
-        <g class="st0">
-          <path class="st1" d="M25.3,312.1L0,279.8c65.8-54.5,140.1-82.6,231-82.6c90.9,0,165.2,28.1,231,82.6l-25.3,32.4
-            c-65.2-46.4-118.6-67.8-205.8-67.8S90.5,265.7,25.3,312.1" />
-          <path class="st1"
-            d="M121.6,0h72.3v69.1h-66.6c-41.7,0-69-10.2-86.7-50.4l27.8-16c12.5,25.6,27.8,31.6,53.2,31.6V0z" />
-          <rect x="292.2" y="0" class="st1" width="72.3" height="69.1" />
-        </g>
-      </svg>
-      <div>
+      <div class="careta">
+        <svg x="0px" y="0px" viewBox="0 0 462 312.1" xml:space="preserve">
+          <g class="st0">
+            <path class="careta-path" d="M25.3,312.1L0,279.8c65.8-54.5,140.1-82.6,231-82.6c90.9,0,165.2,28.1,231,82.6l-25.3,32.4
+              c-65.2-46.4-118.6-67.8-205.8-67.8S90.5,265.7,25.3,312.1" />
+            <path class="careta-path"
+              d="M121.6,0h72.3v69.1h-66.6c-41.7,0-69-10.2-86.7-50.4l27.8-16c12.5,25.6,27.8,31.6,53.2,31.6V0z" />
+            <rect x="292.2" y="0" class="careta-path" width="72.3" height="69.1" />
+          </g>
+        </svg>
+      </div>
+      <div class="error-content">
         <div class="error-number">Error 404</div>
         <h1>Ups...</h1>
-        <p>Sembla que la pàgina que busques ja no està.</p>
-        <a class="apple-link" href="https://www.compromis.net"><span>Torna a la pàgina principal</span></a>
+        <p>Sembla que la pàgina que busques ja no existeix.</p>
+        <a class="apple-link" href="/"><span>Torna a la pàgina principal</span></a>
       </div>
     </div>
   </body>
