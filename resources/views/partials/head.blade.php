ca3b3d2edfccd712c3e5dc2f3c843c8c2a2fe321
Guillem <guillem@disedit.com>
2020-04-15 14:14:14 +0200

    Fix favicon paths

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -8,18 +8,18 @@
   @php wp_head() @endphp
 
   <!-- Favicons -->
-  <link rel="icon" type="image/x-icon" href="@asset('../resources/assets/images/favicons/favicon.ico')">
-  <link rel="icon" type="image/png" sizes="32x32" href="@asset('../resources/assets/images/favicons/favicon-32x32.png')">
-  <link rel="icon" type="image/png" sizes="16x16" href="@asset('../resources/assets/images/favicons/favicon-16x16.png')">
-  <link rel="mask-icon" type="image/x-icon" href="@asset('../resources/assets/images/favicons/safari-pinned-tab.svg')" color="#ff6720">
-  <link rel="apple-touch-icon" sizes="60x60" href="@asset('../resources/assets/images/apple-icon-60x60.png')">
-  <link rel="apple-touch-icon" sizes="76x76" href="@asset('../resources/assets/images/apple-icon-76x76.png')">
-  <link rel="apple-touch-icon" sizes="120x120" href="@asset('../resources/assets/images/apple-icon-120x120.png')">
-  <link rel="apple-touch-icon" sizes="152x152" href="@asset('../resources/assets/images/apple-icon-152x152.png')">
-  <link rel="apple-touch-icon" sizes="180x180" href="@asset('../resources/assets/images/apple-icon-180x180.png')">
-  <link rel="icon" type="image/png" sizes="192x192"  href="@asset('../resources/assets/images/android-icon-192x192.png')">
-  <link rel="icon" type="image/png" sizes="192x192"  href="@asset('../resources/assets/images/android-chrome-192x192.png')">
-  <meta name="msapplication-TileImage" content="@asset('../resources/assets/images/msapplication-icon-144x144.png')">
+  <link rel="icon" type="image/x-icon" href="@asset('images/favicons/favicon.ico')">
+  <link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicons/favicon-32x32.png')">
+  <link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicons/favicon-16x16.png')">
+  <link rel="mask-icon" type="image/x-icon" href="@asset('images/favicons/safari-pinned-tab.svg')" color="#ff6720">
+  <link rel="apple-touch-icon" sizes="60x60" href="@asset('images/favicons/apple-icon-60x60.png')">
+  <link rel="apple-touch-icon" sizes="76x76" href="@asset('images/favicons/apple-icon-76x76.png')">
+  <link rel="apple-touch-icon" sizes="120x120" href="@asset('images/favicons/apple-icon-120x120.png')">
+  <link rel="apple-touch-icon" sizes="152x152" href="@asset('images/favicons/apple-icon-152x152.png')">
+  <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicons/apple-icon-180x180.png')">
+  <link rel="icon" type="image/png" sizes="192x192"  href="@asset('images/favicons/android-icon-192x192.png')">
+  <link rel="icon" type="image/png" sizes="192x192"  href="@asset('images/favicons/android-chrome-192x192.png')">
+  <meta name="msapplication-TileImage" content="@asset('images/favicons/msapplication-icon-144x144.png')">
 
   <!-- Metatags -->
   <meta name="robots" content="index, follow" />
