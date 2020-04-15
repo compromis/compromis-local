80cd50ef6cbf87fc668af71a764ba06bd533687b
Guillem <guillem@disedit.com>
2020-04-15 11:14:00 +0200

    Add og meta tags

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,37 @@
+@php
+  $localInfo = App::get_localinfo();
+@endphp
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
+
+  <!-- Metatags -->
+  <meta name="robots" content="index, follow" />
+  <meta name="keywords" content="compromis, {{ $localInfo->name }}, valencianisme, municipalisme, ecologisme, ecosocialisme, elecciones, eleccions, locales, locals, municipales, municipals" />
+
+  <!-- Open Graph & Twitter -->
+  <meta property="og:locale" content="{{ get_locale() }}" />
+  <meta property="og:site_name" content="Compromís per {{ $localInfo->name }}" />
+  <meta property="twitter:site" content="{{ '@' }}{{ $localInfo->twitter ?: 'compromis' }}" />
+  <meta property="twitter:card" content="summary_large_image" />
+
+  @if(is_single() || is_page())
+    <meta property="og:type" content="article" />
+    <meta property="og:description" content="{!! get_the_excerpt() !!}" />
+    <meta property="og:image" content="{!! get_the_post_thumbnail_url() !!}" />
+    <meta property="og:url" content="{!! get_permalink() !!}" />
+    <meta property="twitter:description" content="{!! get_the_excerpt() !!}" />
+    <meta property="twitter:image" content="{!! get_the_post_thumbnail_url() !!}" />
+    <meta property="twitter:url" content="{!! get_permalink() !!}" />
+  @else
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="A Compromís {{ $localInfo->name }} compartim una nova manera de fer política. Junts podem aconseguir els canvis que el nostre poble necessita." />
+    <meta property="og:image" content="@asset('images/og-image.png')" />
+    <meta property="og:url" content="{!! 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] !!}" />
+    <meta property="twitter:description" content="A Compromís {{ $localInfo->name }} compartim una nova manera de fer política. Junts podem aconseguir els canvis que el nostre poble necessita." />
+    <meta property="twitter:image" content="@asset('images/og-image.png')" />
+    <meta property="twitter:url" content="{!! 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] !!}" />
+  @endif
 </head>
