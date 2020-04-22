8e8a35ba8cf7bb87c295381bd5e1709cfea04e87
Guillem <guillem@disedit.com>
2020-04-22 09:57:27 +0200

    Autofill social widget with col·lectiu usernames

--- app/Widgets/Social.php
+++ app/Widgets/Social.php
@@ -17,7 +17,8 @@ class WP_Widget_Social extends WP_Widget {
     $username = $instance['username'];
     echo $args['before_widget'];
     if ($network === 'facebook') {
-      echo '<div class="fb-page" data-href="https://www.facebook.com/' . $username . '/" data-tabs="" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/' . $username . '/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/' . $username . '/">' . $username . '</a></blockquote></div>';
+      $username = (substr($username, 0, 4) === 'http') ? $username : 'https://www.facebook.com/' . $username;
+      echo '<div class="fb-page" data-href="' . $username . '" data-tabs="" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/' . $username . '/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/' . $username . '/">' . $username . '</a></blockquote></div>';
     } else if($network === 'twitter') {
       echo '<div class="dark-only"><a class="twitter-timeline" data-height="500" data-chrome="noborders nofooter transparent" data-theme="dark" data-dnt="true" href="https://twitter.com/' . $username . '">Tweets by ' . $username . '</a></div>';
       echo '<div class="light-only"><a class="twitter-timeline" data-height="500" data-chrome="noborders nofooter transparent" data-dnt="true" href="https://twitter.com/' . $username . '">Tweets by ' . $username . '</a></div>';
@@ -28,6 +29,9 @@ class WP_Widget_Social extends WP_Widget {
   // Widget Backend 
   public function form($instance)
   {
+    global $wpdb, $blog_id;
+    $info = $wpdb->get_row($wpdb->prepare('SELECT * FROM webs_locals WHERE blogid = %d LIMIT 1', $blog_id));
+
     if (isset($instance['network'])) {
       $network = $instance['network'];
     } else {
@@ -37,7 +41,7 @@ class WP_Widget_Social extends WP_Widget {
     if (isset($instance['username'])) {
       $username = $instance['username'];
     } else {
-      $username = __('', 'sage');
+      $username = $info->facebook;
     }
 
     // Widget admin form
@@ -53,6 +57,16 @@ class WP_Widget_Social extends WP_Widget {
         <label for="<?php echo $this->get_field_id('username'); ?>"><?php _e('Usuari:'); ?></label> 
         <input class="widefat" id="<?php echo $this->get_field_id('username'); ?>" name="<?php echo $this->get_field_name('username'); ?>" type="text" value="<?php echo esc_attr($username); ?>" />
       </p>
+      <script>
+        jQuery("#<?php echo $this->get_field_id('network'); ?>").change(function (e) {
+          var usernameField = jQuery("#<?php echo $this->get_field_id('username'); ?>");
+          if (e.target.value === 'facebook') {
+            usernameField.val('<?php echo $info->facebook; ?>');
+          } else {
+            usernameField.val('<?php echo $info->twitter; ?>');
+          }
+        });
+      </script>
     <?php 
   }
 
