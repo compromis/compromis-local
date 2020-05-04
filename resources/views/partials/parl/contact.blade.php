0f38d528f8119faa5201f5f98c89a3fabc2e659d
Guillem <guillem@disedit.com>
2020-05-04 16:47:02 +0200

    Update contact nav

--- resources/views/partials/parl/contact.blade.php
+++ resources/views/partials/parl/contact.blade.php
@@ -1,6 +1,20 @@
 <div class="card contact-nav">
-  <a class="contact-nav-link" href="#"><i class="fab fa-twitter"></i>@compromis</a>
-  <a class="contact-nav-link" href="#"><i class="fab fa-facebook"></i>compromis</a>
-  <a class="contact-nav-link" href="#"><i class="fab fa-instagram"></i>compromis_net</a>
-  <a class="contact-nav-link" href="#"><i class="far fa-paper-plane"></i>contacta</a>
+  @if($local->twitter)
+    <a class="contact-nav-link" href="https://twitter.com/{{ $local->twitter }}" target="_blank" rel="noopener">
+      <i class="fab fa-twitter"></i> {{ '@' . $local->twitter }}
+    </a>
+  @endif
+  @if($local->facebook)
+  <a class="contact-nav-link" href="{{ $local->facebook }}" target="_blank" rel="noopener">
+    <i class="fab fa-facebook"></i> Facebook
+  </a>
+  @endif
+  @if($local->facebook)
+    <a class="contact-nav-link" href="https://instagram.com/{{ $local->instagram }}" target="_blank" rel="noopener">
+      <i class="fab fa-instagram"></i> {{ '@' . $local->instagram }}
+    </a>
+  @endif
+  <a class="contact-nav-link" href="/contacta">
+    <i class="far fa-paper-plane"></i> Contacta
+  </a>
 </div>
