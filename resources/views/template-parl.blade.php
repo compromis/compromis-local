3e17b229fc3f7466eebfeaa8ee8fbb8b9f78f8ce
Guillem <guillem@disedit.com>
2020-05-04 12:04:44 +0200

    Add oficina parlamentaria

---
+++ resources/views/template-parl.blade.php
@@ -0,0 +1,10 @@
+{{--
+  Template Name: Oficina parlamentaria
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.parl.contact')
+  @include('partials.parl.posts')
+@endsection
