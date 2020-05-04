3415c7af2b882c4a7a09cfa9d40e7bb96338028f
Carles Ibáñez Golfe <carles2612@gmail.com>
2020-05-04 14:39:34 +0200

    Add contact bar to template-parl

---
+++ resources/views/partials/parl/contact.blade.php
@@ -0,0 +1,6 @@
+<div class="card contact-nav">
+  <a class="contact-nav-link" href="#"><i class="fab fa-twitter"></i>@compromis</a>
+  <a class="contact-nav-link" href="#"><i class="fab fa-facebook"></i>compromis</a>
+  <a class="contact-nav-link" href="#"><i class="fab fa-instagram"></i>compromis_net</a>
+  <a class="contact-nav-link" href="#"><i class="far fa-paper-plane"></i>contacta</a>
+</div>
