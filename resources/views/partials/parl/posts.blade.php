517e72a92782e44c873daf6cd45a6c244c39a251
Carles Ibáñez Golfe <carles2612@gmail.com>
2020-05-04 13:57:04 +0200

    Add posts partial for template-parl

---
+++ resources/views/partials/parl/posts.blade.php
@@ -0,0 +1,32 @@
+<h2>Notícies</h2>
+<ul class="card-list">
+  <li>
+    <a href="#" class="entry-link entry-link-highlighted">
+      <div class="entry-thumbnail">
+        <img src="https://source.unsplash.com/random">
+      </div>
+      <div class="entry-info">
+        <h2 class="entry-title">Compromís per Torrent registra una moció per al blindatge legal de la sanitat pública i universal</h2>
+      </div>
+      <div class="entry-bottom">
+        <span>12 abril</span><span class="entry-more"><fa class="far fa-arrow-right" aria-hidden="true"></fa></span>
+      </div>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="entry-link entry-link-highlighted">
+      <div class="entry-thumbnail">
+        <img src="https://source.unsplash.com/random">
+      </div>
+      <div class="entry-info">
+        <h2 class="entry-title">Compromís per Torrent registra una moció per al blindatge legal de la sanitat pública i universal</h2>
+      </div>
+      <div class="entry-bottom">
+        <span>12 abril</span><span class="entry-more"><fa class="far fa-arrow-right" aria-hidden="true"></fa></span>
+      </div>
+    </a>
+  </li>
+</ul>
+<a href="#" class="apple-link">
+  <span>Més notícies</span>
+</a>
