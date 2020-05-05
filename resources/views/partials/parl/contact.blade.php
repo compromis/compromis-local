837b01eeba4061a693b6dd84fc21e883484622d0
Carles Ibáñez Golfe <carles2612@gmail.com>
2020-05-05 12:36:14 +0200

    Style contact bar

--- resources/views/partials/parl/contact.blade.php
+++ resources/views/partials/parl/contact.blade.php
@@ -1,20 +1,36 @@
 <div class="card contact-nav">
-  @if($local->twitter)
-    <a class="contact-nav-link" href="https://twitter.com/{{ $local->twitter }}" target="_blank" rel="noopener">
-      <i class="fab fa-twitter"></i> {{ '@' . $local->twitter }}
+  <div class="contact-nav-name">
+    Oficina <br>Parlamentaria 
+  </div>
+  <div class="contact-nav-address">
+    <a class="contact-nav-link" href="#" target="_blank" rel="noopener">
+      <i class="far fa-home"></i> Carrer Corts Fals, 123
     </a>
-  @endif
-  @if($local->facebook)
-  <a class="contact-nav-link" href="{{ $local->facebook }}" target="_blank" rel="noopener">
-    <i class="fab fa-facebook"></i> Facebook
-  </a>
-  @endif
-  @if($local->facebook)
-    <a class="contact-nav-link" href="https://instagram.com/{{ $local->instagram }}" target="_blank" rel="noopener">
-      <i class="fab fa-instagram"></i> {{ '@' . $local->instagram }}
+    <a class="contact-nav-link" href="#" target="_blank" rel="noopener">
+      <i class="far fa-phone"></i> 96 218 1892
     </a>
-  @endif
-  <a class="contact-nav-link" href="/contacta">
-    <i class="far fa-paper-plane"></i> {{ __('Contacta', 'sage') }}
-  </a>
+    <a class="contact-nav-link" href="#" target="_blank" rel="noopener">
+      <i class="far fa-envelope"></i> corts@compromis.net
+    </a>
+  </div>
+  <div class="contact-nav-network">
+    @if($local->twitter)
+      <a class="contact-nav-link" href="https://twitter.com/{{ $local->twitter }}" target="_blank" rel="noopener">
+        <i class="fab fa-twitter"></i> {{ '@' . $local->twitter }}
+      </a>
+    @endif
+    @if($local->facebook)
+    <a class="contact-nav-link" href="{{ $local->facebook }}" target="_blank" rel="noopener">
+      <i class="fab fa-facebook"></i> Facebook
+    </a>
+    @endif
+    @if($local->facebook)
+      <a class="contact-nav-link" href="https://instagram.com/{{ $local->instagram }}" target="_blank" rel="noopener">
+        <i class="fab fa-instagram"></i> {{ '@' . $local->instagram }}
+      </a>
+    @endif
+  </div>
+    <a class="contact-nav-button" href="/contacta">
+      <i class="far fa-paper-plane"></i> {{ __('Contacta', 'sage') }}
+    </a>
 </div>
