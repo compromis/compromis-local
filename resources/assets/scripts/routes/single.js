78946f6bb91d06f86f8b88895efcb0bcf44498ec
Guillem <guillem@disedit.com>
2020-05-06 13:11:52 +0200

    Add share api support

--- resources/assets/scripts/routes/single.js
+++ resources/assets/scripts/routes/single.js
@@ -12,6 +12,23 @@ export default {
     });
 
     new ClipboardJS('#copy-link');
+
+    $('#share-button').on('click', function (e) {
+      e.preventDefault()
+      if (navigator.share) {
+        navigator.share({
+          title: $(this).data('title'),
+          text: $(this).data('title'),
+          url: $(this).attr('href'),
+        });
+      }
+    });
+
+    $(document).ready(function () {
+      if (navigator.share) {
+        $('#share-button').removeClass('hidden')
+      }
+    })
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -1,12 +1,12 @@
 <div class="social-nav">
-  <a href="{{ the_permalink() }}" id="copy-link" data-url="{{ the_permalink() }}" data-toggle="tooltip" data-placement="top" title="{{ __('Copiar enllaç', 'sage') }}" data-clipboard-text="{{ the_permalink() }}">
+  <a href="{{ get_the_permalink() }}" id="copy-link" data-url="{{ get_the_permalink() }}" data-toggle="tooltip" data-placement="top" title="{{ __('Copiar enllaç', 'sage') }}" data-clipboard-text="{{ the_permalink() }}">
     <i class="far fa-link"></i>
   </a>
-  <a href="https://www.facebook.com/sharer/sharer.php?u={{ the_permalink() }}" target="_blank" rel="noopener">
+  <a href="https://www.facebook.com/sharer/sharer.php?u={{ get_the_permalink() }}" target="_blank" rel="noopener">
     <i class="fab fa-facebook"></i>
     <span class="sr-only">{{ __('Comparteix per Facebook', 'sage') }}</span>
   </a>
-  <a href="https://twitter.com/intent/tweet?text={{ the_title() }} {{ the_permalink() }}" target="_blank" rel="noopener">
+  <a href="https://twitter.com/intent/tweet?text={{ get_the_title() }} {{ get_the_permalink() }}" target="_blank" rel="noopener">
     <i class="fab fa-twitter"></i>
     <span class="sr-only">{{ __('Comparteix per Twitter', 'sage') }}</span>
   </a>
@@ -14,8 +14,12 @@
     <i class="fab fa-whatsapp"></i>
     <span class="sr-only">{{ __('Comparteix per Whatsapp', 'sage') }}</span>
   </a>
-  <a href="mailto:destinatari@exemple.com?&subject={{ the_title() }}&body={{ the_permalink() }}">
+  <a href="mailto:destinatari@exemple.com?&subject={{ get_the_title() }}&body={{ get_the_permalink() }}">
     <i class="far fa-envelope"></i>
     <span class="sr-only">{{ __('Comparteix per e-mail', 'sage') }}</span>
+  </a>
+  <a href="{{ get_the_permalink() }}" class="hidden" id="share-button" data-toggle="tooltip" data-placement="top" title="{{ __('Comparteix', 'sage') }}" data-title="{{ get_the_title() }}">
+    <i class="far fa-share-square"></i>
+    <span class="sr-only">{{ __('Altres serveis', 'sage') }}</span>
   </a>
 </div>
