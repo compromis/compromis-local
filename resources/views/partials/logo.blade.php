f4149efbd12af79130e9ff148d804264549bbd8b
Guillem <guillem@disedit.com>
2020-05-15 11:25:03 +0200

    Minor fixes

--- resources/views/partials/logo.blade.php
+++ resources/views/partials/logo.blade.php
@@ -1,4 +1,4 @@
-<a class="brand {{ is_single() || (is_page() && !is_home()) ? '' : 'brand-compact' }}" href="{{ home_url('/') }}">
+<a class="brand {{ is_single() || (is_page() && !is_front_page()) ? '' : 'brand-compact' }}" href="{{ home_url('/') }}">
   <span class="brand-logo">
     @include('partials.compromis-logo')
     <span class="sr-only">Compromís</span>
